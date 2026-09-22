4f8537f3e1a85cd36946b58498c20d3477c14fb0
Nico Miguelino <nicomiguelino2014@gmail.com>
2026-09-22 08:09:55 -0700

    feat: support import aliases (#8)
    - Add an `@/*` alias for `src/*` in tsconfig.json
    - Replace relative imports in src/ with the alias

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import type { APIContext } from 'astro';
-import { postSlug } from '../lib/posts';
-import { SITE_DESCRIPTION, SITE_NAME } from '../lib/site';
+import { postSlug } from '@/lib/posts';
+import { SITE_DESCRIPTION, SITE_NAME } from '@/lib/site';
 
 export async function GET(context: APIContext) {
   const posts = (await getCollection('blog'))
